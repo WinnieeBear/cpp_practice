b8dd304ba17c9fcc6d89f22e358c46260533d4e4
Winnie <gcf1129@gmail.com>
2021-12-23 16:50:28 +0800

    3_1

--- chap3-The_Semantic_of_Data/3_1_The_Binding_of_Data_Member.h
+++ chap3-The_Semantic_of_Data/3_1_The_Binding_of_Data_Member.h
@@ -2,13 +2,19 @@ extern float x;
 
 class Point3d {
 public:
-    Point3d(float x, float y, float z) : x(x), y(y), z(z) {}
-    float X() const {
+    Point3d(float x, float y, float z) : x(x), y(y), z(z)
+    {
+    }
+    float X() const
+    {
         return x;
     }
-    void X(float new_x) {
-       x = new_x; 
+    void X(float new_x)
+    {
+        // will be the global x
+        x = new_x;
     }
+
 private:
     float x, y, z;
 };
